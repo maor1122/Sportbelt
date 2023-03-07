122fc4dc228fdb4012744fa0e5c20e46935dcdde
name <maorsh74@gmail.com>
2023-03-07 13:53:15 +0200

    A few UI changes + RegisterActivity.

--- app/src/main/java/com/example/sbelt/LoginActivity.java
+++ app/src/main/java/com/example/sbelt/LoginActivity.java
@@ -2,21 +2,17 @@ package com.example.sbelt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.metrics.Event;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.EventListener;
-
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     public void LoginPressed(View v){
